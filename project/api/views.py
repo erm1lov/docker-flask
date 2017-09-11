07807276b7389967f53994eb0a3102c8095a7c8b
tim <oldme@MacBook-Pro-Timofej.local>
2017-09-11 14:02:43 +0300

    add new test method

--- project/api/views.py
+++ project/api/views.py
@@ -6,8 +6,16 @@ users_blueprint = Blueprint('users', __name__)
 
 
 @users_blueprint.route('/ping', methods=['GET'])
-def ping_pong():
+def ping():
     return jsonify({
         'status': 'success',
         'message': 'ping blueprint pong'
+    })
+
+
+@users_blueprint.route('/pong', methods=['GET'])
+def pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'new route and pong ,essage for testing'
     })
